a120417ae0f5bc3a3d99399ec1e25deab94ec74e
fareeha-nahid <fariha.nahid@yahoo.com>
2018-11-23 15:48:25 +1000

    sleep command with method2

--- bashtask.sh
+++ bashtask.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-while sleep 3m; do (./bashtask.sh &); done
+while true
+do
+	var_localcommit=$(git rev-parse HEAD 2>&1)
+	var_remotecommit=$(git rev-parse origin/master 2>&1)
 
-var_localcommit=$(git rev-parse HEAD 2>&1)
-var_remotecommit=$(git rev-parse origin/master 2>&1)
-
-if [ var_localcommit != var_remotecommit ]
-then 
-	echo "Files have been changed since last commit"
-	git pull 
-	git push -u origin master
+	if [ var_localcommit != var_remotecommit ]
+	then 
+		echo "Files have been changed since last commit"
+		git pull 
+		git push -u origin master
 	
-fi
+	fi
+done
