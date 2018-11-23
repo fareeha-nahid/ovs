806ef5b532f4f92e4819ba6a3f6ae6cec5a26dff
fareeha-nahid <fariha.nahid@yahoo.com>
2018-11-23 15:10:48 +1000

    adding bashscript.sh

---
+++ bashtask.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+var_localcommit=$(git rev-parse HEAD 2>&1)
+var_remotecommit=$(git rev-parse origin/master 2>&1)
+
+if [ var_localcommit != var_remotecommit ]
+then 
+	echo "Files have been changed since last commit"
+	git pull 
+	git push -u origin master
+	
+fi
